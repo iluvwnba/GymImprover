be88e7f8585d5a91e7524239ea97cb9bab9fccb3
Martin <Martinpb1l1l1l@gmail.com>
2014-03-23 15:49:27 +0000

    Added instant back navigation and number only keypad to AddFoodPage.xaml

---
+++ GymImprover/GymImprover/AddFoodPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace GymImprover
+{
+    public partial class AddFoodPage : PhoneApplicationPage
+    {
+        public AddFoodPage()
+        {
+            InitializeComponent();
+            this.DataContext = this.DataContext = App.FoodViewModel;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
