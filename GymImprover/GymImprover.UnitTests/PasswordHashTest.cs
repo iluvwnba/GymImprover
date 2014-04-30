1210ce96932e60c3e85a253db483f60a11f11633
Martin Barnes <martinpb1l1l1l@gmail.com>
2014-04-30 23:51:15 +0100

    Added New Unit Tests for Passwordhashing

---
+++ GymImprover/GymImprover.UnitTests/PasswordHashTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GymImprover.Model;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace GymImprover.UnitTests
+{
+    [TestClass]
+    public class PasswordHashTest
+    {
+        private PasswordHash makePasswordHash()
+        {
+            return new PasswordHash();
+        }
+
+        [TestMethod]
+        public void CheckPassword_GoodPassword_ReturnTrue()
+        {
+            PasswordHash ph = makePasswordHash();
+            string salt = ph.GenerateSalt();
+            string password = "password";
+            string hashedPass = ph.HashPassword(password, salt);
+            bool returnFromPasswordCheck = ph.CheckPassword(password, salt, hashedPass);
+            Assert.IsTrue(returnFromPasswordCheck);
+        }
+
+        [TestMethod]
+        public void CheckPassword_BadPassword_ReturnTrue()
+        {
+            PasswordHash ph = makePasswordHash();
+            string salt = ph.GenerateSalt();
+            string password = "password";
+            string badPassword = "badpassword";
+            string hashedPass = ph.HashPassword(password, salt);
+            bool returnFromPasswordCheck = ph.CheckPassword(badPassword, salt, hashedPass);
+            Assert.IsFalse(returnFromPasswordCheck);
+        }
+    }
+}
--- GymImprover/GymImprover/Model/PasswordHash.cs
+++ GymImprover/GymImprover/Model/PasswordHash.cs
@@ -6,7 +6,7 @@ using System.Security.Cryptography;
 
 namespace GymImprover.Model
 {
-    internal class PasswordHash
+    public class PasswordHash
     {
 
         public string GenerateSalt()
